775436dfcb40bbecf0bad0129b27eb77a98f46de
NguyenThiThaoHong <75102265+NguyenThiThaoHong@users.noreply.github.com>
2021-07-26 01:51:52 +0700

    Add files via upload

---
+++ DE1.cpp
@@ -0,0 +1,207 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+struct date
+{
+	int ngay;
+	int thang;
+	int nam;
+};
+struct hocsinh
+{
+	char Hoten[80];
+	date DATE;
+};
+typedef struct hocsinh HOCSINH;
+struct node
+{
+	HOCSINH info;
+	struct node* pNext;
+};
+typedef struct node NODEHS;
+struct list
+{
+	NODEHS* Dau;
+	NODEHS* Cuoi;
+};
+typedef struct list LIST;
+
+/*========KHAI BAO HAM==========*/
+void CreateList(LIST& L);
+NODEHS* CreateHS(HOCSINH x);
+void NhapDate(date& d);
+void XuatDate(date d);
+void NhapHS(HOCSINH& p);
+void XuatHS(HOCSINH p);
+void Nhap(LIST& L);
+void Xuat(LIST L);
+void SapXep(LIST& L);
+void Remove(LIST& L);
+void DS();
+int main()
+{
+	int luachon;
+	NODEHS* HS;
+	LIST l;
+	do
+	{
+		system("cls");
+
+		cout << "DS lua chon: ";
+		DS();
+		cout << "\nNhap lua chon: ";
+		cin >> luachon;
+		switch (luachon)
+		{
+		case 0: break;
+		case 1:
+		{
+			cout << "\Nhap danh sach hoc sinh: ";
+			Nhap(l);
+
+			cout << "\n=======DANHSACH========" << endl;
+			Xuat(l);
+			system("pause");
+			break;
+		}
+		case 2:
+		{
+			cout << "\DS sau khi sap xep: ";
+			SapXep(l);
+			Xuat(l);
+			system("pause");
+			break;
+		}
+		case 3:
+		{
+			cout << "\nHuy danh sach: ";
+			Remove(l);
+			system("pause");
+			break;
+		}
+		default:cout << "VUI LONG NHAP LUA CHON KHAC";
+			break;
+		}
+
+	} while (luachon != 0);
+	return 0;
+	
+}
+
+void CreateList(LIST& L)
+{
+	L.Dau = NULL;
+	L.Cuoi = NULL;
+}
+
+NODEHS* CreateHS(HOCSINH x)
+{
+	NODEHS* p = new NODEHS;
+	if (p == NULL)
+		exit(1);
+	p->info = x;
+	p->pNext = NULL;
+	return p;
+}
+
+void NhapDate(date& d)
+{
+	cout << "\nNhap ngay: ";
+	cin >> d.ngay;
+	cout << "\nNhap thang: ";
+	cin >> d.thang;
+	cout << "\nNhap nam: ";
+	cin >> d.nam;
+}
+
+void XuatDate(date d)
+{
+	cout << d.ngay << "/" << d.thang << "/" << d.nam;
+}
+
+void NhapHS(HOCSINH& p)
+{
+	cin.ignore();
+	cout << "\nNhap ho ten: ";
+	cin >> p.Hoten;
+	cout << "\nNhap ngay sinh: ";
+	NhapDate(p.DATE);
+}
+
+void XuatHS(HOCSINH p)
+{
+	cout << p.Hoten << "|";
+	XuatDate(p.DATE);
+}
+void AddHead(LIST& l, NODEHS* p)
+{
+	if (l.Dau == NULL)
+	{
+		l.Dau = p;
+		l.Cuoi = l.Dau;
+	}
+	else
+	{
+		p->pNext = l.Dau;
+		l.Dau= p;
+	}
+}
+void Nhap(LIST& L)
+{
+	int n;
+	cout << "\n Nhap so phan tu cua DS ";
+	cin >> n;
+	CreateList(L);
+	for (int i = 1; i <= n; i++)
+	{
+		HOCSINH x;
+		NhapHS(x);
+		NODEHS* p = CreateHS(x);
+		if (p != NULL)
+			AddHead(L, p);
+	}
+}
+
+void Xuat(LIST L)
+{
+	NODEHS* p = L.Dau;
+	while (p != NULL)
+	{
+		XuatHS(p->info);
+		p = p->pNext;
+		cout << endl;
+	}
+}
+
+void SapXep(LIST& L)
+{
+	HOCSINH t;
+	for (NODEHS *p=L.Dau;p!=NULL;p=p->pNext)
+		for (NODEHS *q=p->pNext;q!=NULL;q=q->pNext)
+			if (strcmp(p->info.Hoten, q->info.Hoten) > 0)
+			{
+				t = p->info;
+				p->info = q->info;
+				q->info = t;
+			}
+}
+
+void Remove(LIST& L)
+{
+	NODEHS* t;
+	while (L.Dau!=NULL)
+	{
+		t = L.Dau;
+		L.Dau = t->pNext;
+		delete t;
+
+	}
+}
+void DS()
+{
+	cout << "\n0.Thoat man hinh.";
+	cout << "\n1.Nhap & Xuat danh sach.";
+	cout << "\n2.Sap xep danh sach.";
+	cout << "\n3.Huy danh sach.";
+}
---
+++ DE2.cpp
@@ -0,0 +1,176 @@
+#include<iostream>
+using namespace std;
+
+struct node
+{
+	int info;
+	struct node* pLeft;
+	struct node* pRight;
+};
+typedef struct node NODE;
+typedef NODE* TREE;
+
+void CreateTree(TREE &T)
+{
+	T = NULL;
+}
+NODE* Createnode(int x)
+{
+	NODE* p = new NODE;
+	p->info = x;
+	p->pLeft = NULL;
+	p->pRight = NULL;
+	return p;
+}
+void AddNode(TREE& T, NODE* p)
+{
+	if (T)
+	{
+		if (T->info == p->info)
+			return;
+		if (T->info > p->info)
+			AddNode(T->pLeft, p);
+		else
+			AddNode(T->pRight, p);
+	}
+	else
+		T = p;
+
+}
+void Input(TREE &T)
+{
+	int n, x;
+	cout << "\nNhap so luong node: ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "\nNhap node thu " << i + 1 << ":";
+		cin >> x;
+		NODE* p = Createnode(x);
+		AddNode(T, p);
+	}
+
+}
+
+void Duyet_RNL(TREE T)
+{
+	//neu cay con phan tu thi duyet
+	if (T != NULL)
+	{
+		Duyet_RNL(T->pRight);//duyet qua trai;
+		cout << T->info << " ";//xuat gia tri cua node;
+		Duyet_RNL(T->pLeft);//duyet qua phai;
+	}
+}
+int Node_la(TREE T)
+{
+	if (T == NULL)
+		return 0;
+	else if (T->pLeft == NULL && T->pRight == NULL)
+		return 1;
+	return Node_la(T->pLeft) + Node_la(T->pRight);
+}
+bool SoNguyenTo(int x)
+{
+	int dem = 0;
+	for (int i = 1; i < x; i++)
+		if (x % i == 0)
+			dem++;
+	if (dem == 2)
+		return true;
+	return false;
+}
+void SearchStandFor(TREE& pHuy, TREE& pTM)
+{
+	if (pHuy->pLeft)
+		SearchStandFor(pHuy, pTM->pLeft);
+	else
+	{
+		pHuy->info = pTM->info;
+		pHuy = pTM;
+		pTM = pTM->pRight;
+	}
+
+}
+int XoaNode(TREE& T, int x)
+{
+	if (T)
+	{
+		if (x < T->info)
+			XoaNode(T->pLeft, x);
+		if (x > T->info)
+			XoaNode(T->pRight, x);
+		else
+		{
+			NODE* pHuy = T;
+			if (T->pLeft == NULL)
+				T = T->pRight;
+			else if (T->pRight == NULL)
+				T = T->pLeft;
+			else SearchStandFor(pHuy, T->pRight);
+			delete pHuy;
+			return 1;
+		}
+	}
+	else
+		return 0;
+}
+void XoaNodeSNT(TREE& T)
+{
+	if (T)
+	{
+		XoaNodeSNT(T->pLeft);
+		XoaNodeSNT(T->pRight);
+		if (SoNguyenTo(T->info) == true)
+			XoaNode(T, T->info);
+	}
+}
+void MENU()
+{
+	cout << "\n0.Thoat man hinh.";
+	cout << "\n1.Tao cay nhi phan tim kiem.";
+	cout << "\n2.Duyet theo thu tu giam dan.";
+	cout << "\n3.Xoa node la so nguyen to.";
+}
+int main()
+{
+	TREE T;
+	CreateTree(T);
+	int x, luachon;
+	do {
+		system("cls");
+
+		MENU();
+		cout << "\nNhap lua chon: ";
+		cin >> luachon;
+		switch (luachon)
+		{
+		case 0:break;
+		case 1:
+		{
+			cout << "\nTao cay nhi phan tim kiem: ";
+			Input(T);
+			system("pause");
+			break;
+		}
+		case 2:
+		{
+			cout << "\nDuyet cay theo thu tu giam dan: ";
+			Duyet_RNL(T);
+			system("pause");
+			break;
+		}
+		case 3:
+		{
+			cout << "\nXoa node la so nguyen to: ";
+			XoaNodeSNT(T);
+			Duyet_RNL(T);
+			system("pause");
+			break;
+		}
+		default:cout << "VUI LONG NHAP LAI LUA CHON";
+			break;
+		}
+	} while (luachon != 0);
+	return 0;
+}
